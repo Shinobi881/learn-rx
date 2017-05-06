2957eaa54894de43fe7bc3a474b52f0cc2780743
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-06 00:50:16 -0700

    (#24) implement zip #4

--- rx-functions.js
+++ rx-functions.js
@@ -813,7 +813,24 @@ function reduceInsteadOfFilter() {
 	//	 {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" },
 	//	 {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
 	// ];
-
+	
+	// return movieLists.
+	// 	concatMap(({ videos }) => {
+	// 		return videos.concatMap(({ id, title, boxarts }) => {
+	// 			return boxarts.reduce((acc, next) => {
+	// 				let boxart = '';
+	// 				const { width: accWidth, url: accUrl } = acc, 
+	// 						{ width: nextWidth, url: nextUrl } = next;
+	// 				
+	// 				if (accWidth) boxart = accWidth < nextWidth ? acc : next;
+	// 				
+	// 				return boxart;
+	// 			}).map(({ url: boxart }) => {
+	// 				return { id, title, boxart };
+	// 			});			
+	// 		});
+	// 	});
+	
 	return movieLists.
 		concatMap(({ videos }) => {
 			return videos.concatMap(({ id, title, boxarts }) => {
@@ -830,27 +847,67 @@ function reduceInsteadOfFilter() {
 		});
 }
 
-// const newMovies = movieLists.concatMap(({ videos }) => {
-// 	return videos.concatMap(video => {
-// 		const { id, title, boxarts } = video;
-// 		
-// 		return boxarts
-// 			.filter(({ width }) => width === 150)
-// 			.map(({ url: boxart }) => {
-// 				return { id, title, boxart };
-// 			});
-// 	});
-// });
-// 
-// .map(video => {
-// const { id, title, boxarts } = video;
-// // console.log('List: ', list.boxarts.reduce((art)));
-// return { id, title, boxart: boxarts.reduce((acc, next) => {
-// 	return acc.width < next.width ? acc.url : next.url;
-// }) };
-// });
-
-
-console.log('Reduced: ', reduceInsteadOfFilter());
+// console.log('Reduced: ', reduceInsteadOfFilter());
 // reduceInsteadOfFilter();
+
+/*
+Zipping Arrays:
+
+Sometimes we need to combine two arrays by progressively taking an item from each and combining the 
+pair. If you visualize a zipper, where each side is an array, and each tooth is an item, you'll have 
+a good idea of how the zip operation works.
+
+Exercise 21: Combine videos and bookmarks by index
+
+Use a for loop to traverse the videos and bookmarks array at the same time. For each video and 
+bookmark pair, create a {videoId, bookmarkId} pair and add it to the videoIdAndBookmarkIdPairs array.
+*/
+
+function zip() {
+	var videos = [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			},
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			}
+		],
+		bookmarks = [
+			{id: 470, time: 23432},
+			{id: 453, time: 234324},
+			{id: 445, time: 987834}
+		],
+	counter,
+	videoIdAndBookmarkIdPairs = [];
+
+	for(counter = 0; counter < Math.min(videos.length, bookmarks.length); counter++) {
+		// Insert code here to create a {videoId, bookmarkId} pair and add it to the videoIdAndBookmarkIdPairs array.
+	}
+
+	return videoIdAndBookmarkIdPairs;
+}
+
+console.log('Zipping: ', zip());
 		
