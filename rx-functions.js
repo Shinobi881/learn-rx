d4cb61b1229ecd903ee80d9902cd0eddeea9892b
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-05-06 00:47:04 -0700

    (#20) impplement reduceInsteadOfFilter close #3

--- rx-functions.js
+++ rx-functions.js
@@ -735,5 +735,122 @@ function reduceBoxarts() {
 		}).map(art => art.url);   // Complete this expression
 }
 
-console.log(reduceBoxarts());
+// console.log(reduceBoxarts());
+
+/*
+Exercise 20: Retrieve the id, title, and smallest box art url for every video.
+
+This is a variation of the problem we solved earlier, where we retrieved the url of the boxart with 
+a width of 150px. This time we'll use reduce() instead of filter() to retrieve the smallest box art 
+in the boxarts array. 
+*/
+
+function reduceInsteadOfFilter() {
+	var movieLists = [
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxarts": [
+						{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+						{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxarts": [
+						{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+						{ width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id:432534, time:65876586 }]
+				}
+			]
+		},
+		{
+			name: "Thrillers",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxarts": [
+						{ width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+						{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxarts": [
+						{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+						{ width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+						{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id:432534, time:65876586 }]
+				}
+			]
+		}
+	];
+
+
+	// Use one or more concatMap, map, and reduce calls to create an array with the following items (order doesn't matter)
+	// [
+	//	 {"id": 675465,"title": "Fracture","boxart":"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+	//	 {"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+	//	 {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" },
+	//	 {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+	// ];
+
+	return movieLists.
+		concatMap(({ videos }) => {
+			return videos.concatMap(({ id, title, boxarts }) => {
+				return boxarts.reduce((acc, next) => {
+					const { width: accWidth, url: accUrl } = acc, 
+							{ width: nextWidth, url: nextUrl } = next;
+					const boxart = accWidth < nextWidth ? accUrl : nextUrl;
+					
+					if (accWidth) test = { id, title, boxart };
+					
+					return test;
+				});			
+			});
+		});
+}
+
+// const newMovies = movieLists.concatMap(({ videos }) => {
+// 	return videos.concatMap(video => {
+// 		const { id, title, boxarts } = video;
+// 		
+// 		return boxarts
+// 			.filter(({ width }) => width === 150)
+// 			.map(({ url: boxart }) => {
+// 				return { id, title, boxart };
+// 			});
+// 	});
+// });
+// 
+// .map(video => {
+// const { id, title, boxarts } = video;
+// // console.log('List: ', list.boxarts.reduce((art)));
+// return { id, title, boxart: boxarts.reduce((acc, next) => {
+// 	return acc.width < next.width ? acc.url : next.url;
+// }) };
+// });
+
+
+console.log('Reduced: ', reduceInsteadOfFilter());
+// reduceInsteadOfFilter();
 		
